868790bec8112e9d1a049ddf06fba2043bf5a2e0
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-02 21:42:49 +0800

    first commit

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Stateful',
+      home: Scaffold(
+        body: Text('Login Stateful'),
+      ),
+    );
+  }
+}
